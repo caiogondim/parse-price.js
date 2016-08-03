4ceb1f08105d4132f2b877c53602097337e1d816
Caio Gondim <me@caiogondim.com>
2016-08-03 21:24:55 +0200

    1.1.3

--- dist/parse-price.min.js
+++ dist/parse-price.min.js
@@ -1,7 +1,7 @@
 /**
- * parse-price - returns a `Number` from a localized price string
+ * parse-price - returns a Number from a localized price string
  *
- * @version 1.1.2
+ * @version 1.1.3
  * @link https://github.com/caiogondim/parse-price.js#readme
  * @author Caio Gondim
  * @license MIT
