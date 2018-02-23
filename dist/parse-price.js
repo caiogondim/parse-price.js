cd5ca2b060a2f16ad4a059aabfa96374e7ec4d2f
Caio Gondim <me@caiogondim.com>
2018-02-23 11:42:49 -0500

    1.1.6

--- dist/parse-price.js
+++ dist/parse-price.js
@@ -1,7 +1,7 @@
 /**
  * parse-price - returns a Number from a localized price string
  *
- * @version 1.1.5
+ * @version 1.1.6
  * @link https://github.com/caiogondim/parse-price.js#readme
  * @author Caio Gondim
  * @license MIT
