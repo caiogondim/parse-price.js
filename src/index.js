0b0276530e5d30eef38b2b4b33c76bdf85365ea6
Floris Vink <fvink@me.com>
2018-02-24 19:02:44 +0100

    code style fixes

--- src/index.js
+++ src/index.js
@@ -15,21 +15,21 @@ function filterNumbersDotsAndCommas (str) {
 }
 
 function getDecimalSymbol (str) {
-
-  var str_filtered = filterNumbersDotsAndCommas(str)
-  var endWithZero = str_filtered[str_filtered.length - 1] === '0'
+  var strFiltered = filterNumbersDotsAndCommas(str)
+  var endWithZero = strFiltered[strFiltered.length - 1] === '0'
 
     // for each character starting from the end...
-  for (var i = str_filtered.length; i > 0; i--) {
-
+  for (var i = strFiltered.length; i > 0; i--) {
       // if the last character is a "0" and the decimal position > 3, no decimal
-    if (((str_filtered.length - i + 1) > 3) && endWithZero)
+    if (((strFiltered.length - i + 1) > 3) && endWithZero) {
       return
+    }
 
-    var currentChar = str_filtered[i - 1]
+    var currentChar = strFiltered[i - 1]
 
-    if ([',', '.'].indexOf(currentChar) !== -1)
+    if ([',', '.'].indexOf(currentChar) !== -1) {
       return currentChar
+    }
   }
 }
 
@@ -42,14 +42,14 @@ function getDecimalSymbol (str) {
  * @return {Number}
  */
 function parsePrice (input) {
-
   var str = String(input)
 
   var decimalPart = '00'
   var decimalSymbol = getDecimalSymbol(str)
 
-  if (decimalSymbol)
+  if (decimalSymbol) {
     decimalPart = str.split(decimalSymbol)[1]
+  }
 
   var integerPart = str.split(decimalSymbol)[0]
 
